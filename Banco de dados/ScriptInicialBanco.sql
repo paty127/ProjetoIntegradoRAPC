2ce720a68f7a7a2edacbe7c9dc31f7ab32b051d3
alexsandrosr88 <63133361+alexsandrosr88@users.noreply.github.com>
2021-05-21 03:43:42 -0300

    Acertado procedure novo_adm

--- Banco de dados/ScriptInicialBanco.sql
+++ Banco de dados/ScriptInicialBanco.sql
@@ -68,12 +68,34 @@ CREATE TABLE Adm (
     rg varchar(13),
     cpf varchar(16),
     celular varchar(15),
+    email varchar(40),
     senha varchar(60) not null,
     senha_repetida varchar(60) not null,
 	perfil varchar(14),
     fk_endereco integer,
     FOREIGN KEY (fk_endereco) REFERENCES endereco (id_endereco)
 );
+
+delimiter $$
+create procedure novo_adm (
+nome varchar(50),sexo varchar(9),data_nascimento date,
+rg varchar(13),cpf varchar(16),celular varchar(15),  
+email varchar(40),senha varchar(60), senha_repetida varchar(60),perfil varchar(14),
+rua varchar(30),numero integer,complemento varchar(30),bairro varchar(30),cep varchar(9))
+    begin
+	insert into Endereco(rua,numero,complemento,bairro,cep)values(rua,numero,complemento,bairro,cep);
+    insert into Adm(nome,sexo,data_nascimento,rg,cpf,celular,email,perfil,senha,senha_repetida,fk_endereco)values(nome,sexo,data_nascimento,rg,cpf,celular,email,perfil,senha,senha_repetida,@@identity);
+    end $$
+
+Exemplo de criação do usuário com a procedure
+call novo_adm ('Fulano','Masculino','1988/08/14','11.111.111-1','111.1111.111-11','1196291-0587','fulano@msn.com','12345678','12345678','administrativo','Avenida Circular', 113,'Apto52 Bloco6','Jardim Raposo','05547-025');
+
+Exemplo de consula:
+select *
+FROM Adm as A
+JOIN endereco as E on A.fk_endereco = E.id_endereco;
+*/
+
 
 CREATE TABLE Professor (
     cod_professor integer not null auto_increment PRIMARY KEY,
@@ -166,11 +188,15 @@ aluno.sexo = 'M' where cod_aluno = 4;
 UPDATE aluno INNER JOIN endereco ON aluno.fk_endereco = id_endereco SET endereco.numero = 100 WHERE cod_aluno = 7;
 
 */
-/*
+
 use projetorapc;
 drop database projetorapc;
 
 select *
 FROM aluno as A
 JOIN endereco as E on A.fk_endereco = E.id_endereco;
+select *
+FROM professor as A
+JOIN endereco as E on A.fk_endereco = E.id_endereco;
+
 */
